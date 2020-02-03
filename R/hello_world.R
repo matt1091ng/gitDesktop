a4e4e3c3c4505d3036837c8e91e842aa56381269
Matt N Tran <nnguye44@jhmi.edu>
2020-02-03 14:46:10 -0500

    Learning git with hello_world.R
    Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>

---
+++ R/hello_world.R
@@ -0,0 +1,17 @@
+## Avoid this:
+foo <- function() {
+  print('message')
+  2 + 2
+}
+
+## Do this instead:
+foo2 <- function() {
+  message(paste(Sys.time(), 'message'))
+  2 + 2
+}
+
+## So you can use suppressMessages()
+foo()
+foo2()
+suppressMessages(foo())
+suppressMessages(foo2())
