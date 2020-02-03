990d13e832317cc3a2fb5d1361d8212858712426
Matt N Tran <nnguye44@jhmi.edu>
2020-02-03 14:54:14 -0500

    Showcasing here::here()
    Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>

---
+++ R/use_here_for_relative_paths.R
@@ -0,0 +1,11 @@
+
+
+## Test some relative code
+file_path <- here::here('R', 'hello_world.R')
+
+## Works in any computer ^^
+source(file_path)
+
+## Doesn't work outside of this laptop
+file_path <-
+  file.path('/Users/Matt/Desktop/gitDesktop/', 'R', 'hello_world.R')
